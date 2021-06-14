2aabfbc617e48a52fb51022b7720984f59bb2aa2
danilrekt <80561640+danilrekt@users.noreply.github.com>
2021-06-14 22:31:49 +0500

    Update MyProject4-HTML5-Shipping.data.js

--- MyProject4-HTML5-Shipping.data.js
+++ MyProject4-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/danil/Documents/Unreal Projects/MyProject4/Binaries/HTML5/MyProject4-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject4-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/a3orhe5hvygrmb6/MyProject4-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/a3orhe5hvygrmb6/MyProject4-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
